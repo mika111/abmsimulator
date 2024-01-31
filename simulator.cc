2e41a3e24a21ed17ae0f25eabffd59fbfc23d84f
mika111 <49643223+mika111@users.noreply.github.com>
2024-01-31 20:38:17 +0000

    1

--- simulator.cc
+++ simulator.cc
@@ -5,18 +5,18 @@
 #include <vector>
 int main()
 {
-    SimulationEnv simenv;
-    simenv.initprey();
+    SimulationEnv simenv(321,123);
+    simenv.initprey(100);
     Agent *pointer = new Prey();
     std::vector<std::thread> agentthreads;
     std::vector<Agent*> agents;
-    for(int i = 0;i <=100;i++)
-    
-    {Agent *agentptr = new Prey();
-    agents.emplace_back(agentptr);}
+    for(int i = 0;i <=100;i++) 
+    {
+    Agent *agentptr = new Prey();
+    agents.emplace_back(agentptr);
+    }
     for (int j = 0;j<=100;j++)
-
     {std::cout << agents[j]->agenttype()<< " " << j << std::endl;}
-
+    std::cout << simenv.x << " " << simenv.y<< std::endl;
     return 1;
 }
